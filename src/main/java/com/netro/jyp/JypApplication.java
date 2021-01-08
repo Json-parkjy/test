b9c31a417ccd96506d9c1b73ac2e5194fe170baa
jyp <pjy0011a@naver.com>
2021-01-08 17:06:58 +0900

    init

---
+++ src/main/java/com/netro/jyp/JypApplication.java
@@ -0,0 +1,13 @@
+package com.netro.jyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JypApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netro/jyp/JypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netro.jyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
