65b4202b62af351bd4c59d1f329a95e44a1f6c62
jyp <pjy0011a@naver.com>
2021-01-12 18:06:17 +0900

    Add validation

--- src/main/java/com/netro/jyp/controller/BoardController.java
+++ src/main/java/com/netro/jyp/controller/BoardController.java
@@ -1,15 +1,49 @@
 package com.netro.jyp.controller;
 
+import com.netro.jyp.model.Board;
+import com.netro.jyp.repository.Boardrepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private Boardrepository boardrepository;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> boards = boardrepository.findAll();
+        model.addAttribute("boards", boards);
         return "board/list";
     }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false)Long id) {
+        if(id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardrepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String boardSubmit(@Valid Board board, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardrepository.save(board);
+        return "redirect:/board/list";
+    }
+
 }
