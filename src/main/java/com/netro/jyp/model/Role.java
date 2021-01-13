9f00d9647b3c406202306a51bb872c1a4b84b804
jyp <pjy0011a@naver.com>
2021-01-13 18:34:19 +0900

    Add Login editing....

---
+++ src/main/java/com/netro/jyp/model/Role.java
@@ -0,0 +1,20 @@
+package com.netro.jyp.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users = new ArrayList<>();
+}
---
+++ src/main/java/com/netro/jyp/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.netro.jyp.repository;
+
+import com.netro.jyp.model.Board;
+import com.netro.jyp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
